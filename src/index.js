ba743347ec412fe3afbb78901a3ba4cdc4fd4459
Dpons039 <dpons@outlook.com>
2022-06-16 23:31:58 +0200

    Capturar entrada del usuario

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import { printHello } from '#Lib/hello';
+import { promptQuestion } from '#Lib/promptQuestion';
 
-printHello();
+
+(async () => {
+    // 1. Capturar entrada usuario
+    const userAnswer = await promptQuestion('Introduce tu operación: \n');
+    console.log(userAnswer);
+
+    // 2. validar la entrada
+
+    // 3. realizar la operación
+
+    // 4. mostrar resultado
+})();
