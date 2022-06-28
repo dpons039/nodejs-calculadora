4d95319ba11763118903179924c6a8de4534923b
Dpons039 <dpons@outlook.com>
2022-06-28 19:28:48 +0200

    Refactor, tramiento de errores y actualizaciones

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import { promptQuestion } from '#Lib/promptQuestion';
-
+import { InvalidInputError } from '#Errors/invalidInputError.js';
+import { getOperator } from '#Lib/getOperator.js';
+import { promptQuestion } from '#Lib/promptQuestion.js';
 
 (async () => {
-    // 1. Capturar entrada usuario
-    const userAnswer = await promptQuestion('Introduce tu operación: \n');
-    console.log(userAnswer);
+    try {
+        // 1. Capturar entrada usuario
+        const userAnswer = await promptQuestion('Introduce tu operación: ');
 
-    // 2. validar la entrada
+        // 2. validar la entrada
+        const standarizeInput = userAnswer.trim();
+
+        if (standarizeInput === '') {
+            throw new InvalidInputError();
+        }
+        const operator = getOperator(standarizeInput);
+
+        if (!operator) {
+            throw new InvalidInputError();
+        }
+    } catch (e) {
+        if (e instanceof InvalidInputError) console.log(e.message);
+        else
+            console.log(`Error no controlado: ${e.message}. Stack: ${e.stack}`);
+    }
 
     // 3. realizar la operación
 
